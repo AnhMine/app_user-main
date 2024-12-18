1f4b5f80af42def4749c5816464bed60f72e967e
Huy Ho <azirkingsand0.12@gmail.com>
2024-12-18 14:27:55 +0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,9 @@ module.exports = {
     },
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
+  output: {
+    path: path.resolve(__dirname, "dist"), // Thư mục đầu ra của các tệp build
+    filename: "[name].bundle.js", // Định dạng tên tệp của các bundle
+    publicPath: "/assets/", // Đảm bảo các tài nguyên tĩnh sẽ được phục vụ từ /assets/
+  },
 };
